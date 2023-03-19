3b336a7f5e5d307712f7f57b40f5803ca1350b7c
whiskey254 <willykipchumba21@gmail.com>
2023-03-19 04:32:24 -0700

    willy:update

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	printf("export CFILE=0-main.c ; ./0-preprocessor");
-    return (0);
+	printf("Hello World!");
+	return (0);
 }
